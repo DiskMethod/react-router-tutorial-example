38c812f39e0e1e1a26de33fa3482929487dda9c4
Lucas Chagas <lucas.w.chagas@gmail.com>
2022-06-22 17:08:58 -0400

    Route Config

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
 import "./App.css";
 
 function App() {
-  return;
+  return (
+    <div>
+      <h1>Bookkeeper!</h1>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
+    </div>
+  );
 }
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import App from "./App";
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
+
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="expenses" element={<Expenses />} />
+        <Route path="invoices" element={<Invoices />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
